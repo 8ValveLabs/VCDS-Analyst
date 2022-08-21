62adc906f64d1ad09472a49ea02c87747be58f5b
nashm <nash.michel@hotmail.com>
2022-08-21 18:31:39 -0400

    Minor work on formatting of header Trying to find a solution to removing the EOL characters that are getting stuck to the end of the header list.

--- file_parser.py
+++ file_parser.py
@@ -1,3 +1,4 @@
+from array import array
 import os, re
 
 def prompt_file():
@@ -7,18 +8,32 @@ def filter_prompt(prompt):
     return prompt.strip()
 
 def scan_header(filtered_path):
-    header_start_pos = 0
+    header = {}
     with open(filtered_path, encoding="utf-8") as file_opened:
         scan_line = file_opened.readline()
         while (scan_line.startswith('Marker') != True):
             scan_line = file_opened.readline()
-        header_start_pos = file_opened.tell()
-    return header_start_pos
+        header = [
+            scan_line,
+            file_opened.readline(),
+            file_opened.readline()
+        ]
+    return header
+
+def format_header(header):
+    for count, value in enumerate(header):
+        header[count] = value.split(',')
+        
+    for count, value in enumerate(header[1]):
+        header[0][count]+= f' {value} {header[2][count]}'
+    
 
 
 if __name__ == "__main__":
-    scan_header(
-    filter_prompt(
-        prompt_file()
+    format_header(
+        scan_header(
+            filter_prompt(
+                prompt_file()
+            )
+        )
     )
-   )
