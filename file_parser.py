b8f620dc4630535be6d86fe92f402c6e93f6a812
nashm <nash.michel@hotmail.com>
2022-09-03 13:27:06 -0400

    Header Parsing Header of a log file is now successfully formatted, split up, and saved as a python Dict. This will make it a lot easier to query info about the car, vcds sofware info, and will allow for identification of which sensors were logged. Added some simple try-except blocks for error handling. Must test with more log files, currently untested.

--- file_parser.py
+++ file_parser.py
@@ -7,17 +7,23 @@ def prompt_file():
 def filter_prompt(prompt):
     return prompt.strip()
 
-def scan_header(filtered_path):
+def cut_header(filtered_path):
     header = {}
     with open(filtered_path, encoding="utf-8") as file_opened:
-        scan_line = file_opened.readline()
-        while (scan_line.startswith('Marker') != True):
-            scan_line = file_opened.readline()
-        header = [
-            scan_line,
-            file_opened.readline(),
-            file_opened.readline()
-        ]
+        timestamp = file_opened.readline()
+        carinfo = file_opened.readline()
+        sensors = None
+        marker_found = False
+        while (marker_found == False):
+            scanline = file_opened.readline()
+            if (scanline.startswith("Marker")):
+                sensors = scanline, file_opened.readline()
+                marker_found = True
+    header = {
+        'timestamp' : format_timestamp(timestamp),
+        'carinfo' : format_carinfo(carinfo),
+        'sensors' : format_sensors(sensors)
+    }
     return header
 
 def count_markers(filtered_path: str) -> int:
@@ -27,22 +33,55 @@ def count_markers(filtered_path: str) -> int:
             count+=1     
     return count
 
-def format_header(header):
-    if header == [""]: 
-        pass
-    for count, value in enumerate(header):
-        header[count] = value.split(',')
-        
-    for count, value in enumerate(header[1]):
-        header[0][count]+= f' {value} {header[2][count]}' """ """
-    
+def format_sensors(sensors: dict) -> dict:
+    try:
+        slice = sensors[0].split(','), sensors[1].split(',')
+        sensors_formatted = {}
+        for count, value in enumerate(slice[0]):
+            if (value):
+                sensors_formatted[count] = f'{value} {slice[1][count]}'
+        sensors_formatted.popitem()
+    except:
+        print(f"Error reading file, sensor marker failed to format\n")
+        sensors_formatted = None
+    finally:
+        return sensors_formatted
+
+def format_carinfo(carinfo: dict) -> dict:
+    try:
+        sliced = carinfo.split(',')
+        carinfo = {
+            'chassis_code' : sliced[0],
+            'platform_code' : sliced[1],
+            'ecu_info' : sliced[2]
+        }
+    except:
+        print("Error reading file, second line failed to format.")
+    finally:
+        return carinfo
+
+def format_timestamp(timestampinfo: dict) -> dict:
+    try:
+        sliced = timestampinfo.split(',')
+        timestamp = {
+            'day_name' : sliced[0],
+            'day' : sliced[1],
+            'month_name' : sliced[2],
+            'year' : sliced[3],
+            'vcid' : sliced[4],
+            'vcds_version' : sliced[5],
+            'data_version' : sliced[6].rstrip()
+        }
+    except:
+        print("Error reading file, first line could not be formatted.")
+        sliced = None
+    finally:
+        return timestamp
 
 
 if __name__ == "__main__":
-    format_header(
-        scan_header(
-            filter_prompt(
-                prompt_file()
-            )
+    print(cut_header(
+        filter_prompt(
+            prompt_file()
         )
-    )
+    ))
